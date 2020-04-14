be3904598fed39be1f2c2b20e54a0c1c25a4a682
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 23:26:33 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array = array.push(string)
+end
+
+def using_unshift(array, string)
+  array = array.unshift(string)
+end
+
+def using_pop(array)
+  array = array.pop()
+end
+
+def pop_with_args(array)
+  array = array.pop(2)
+end
+
+def using_shift(array)
+  array = array.shift()
+end
+
+def shift_with_args(array)
+  array = array.shift(2)
+end
